09bd2205c133556b1bc8787fdc4b3048c7fef3c9
Esha Patel <Patellesha11@gmail.com>
2025-03-28 15:26:19 +0300

    added counter increase

--- src/Counter.js
+++ src/Counter.js
@@ -3,7 +3,10 @@ import react, {useState}from "react";
 function Counter () {
 
     const [counter, setCounter] = useState(0);
-    return (
+
+
+    const increaseCount = () => setCounter(count + 1);   
+     return (
         <div>
           <h1>Counter App</h1>
 
